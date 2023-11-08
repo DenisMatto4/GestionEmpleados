75a3254dc1745dac52ce314f82071dfdc77e77f3
DenisMatto4 <denis.matto.29@gmail.com>
2023-11-08 13:37:24 +0100

    Añadido la nueva pantalla de employees con linq, se puede filtar con los tres filtros

---
+++ AppEmpleados/EmployeeWFLinq.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AppEmpleados
+{
+    partial class EmployeeWFLinq
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxCiudad = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridViewEmployees = new System.Windows.Forms.DataGridView();
+            this.dtLocations = new AppEmpleados.dtLocations();
+            this.locationsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.locationsTableAdapter = new AppEmpleados.dtLocationsTableAdapters.locationsTableAdapter();
+            this.btnRestablecer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtLocations)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locationsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(32, 74);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(167, 20);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(32, 135);
+            this.txtApellidos.Margin = new System.Windows.Forms.Padding(2);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(167, 20);
+            this.txtApellidos.TabIndex = 4;
+            this.txtApellidos.TextChanged += new System.EventHandler(this.txtApellidos_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Apellidos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 168);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ciudad";
+            // 
+            // comboBoxCiudad
+            // 
+            this.comboBoxCiudad.DataSource = this.locationsBindingSource;
+            this.comboBoxCiudad.DisplayMember = "city";
+            this.comboBoxCiudad.FormattingEnabled = true;
+            this.comboBoxCiudad.Location = new System.Drawing.Point(32, 186);
+            this.comboBoxCiudad.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxCiudad.Name = "comboBoxCiudad";
+            this.comboBoxCiudad.Size = new System.Drawing.Size(167, 21);
+            this.comboBoxCiudad.TabIndex = 7;
+            this.comboBoxCiudad.ValueMember = "location_id";
+            this.comboBoxCiudad.SelectedValueChanged += new System.EventHandler(this.comboBoxCiudad_SelectedValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 24);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Filtrar los datos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(261, 55);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Lista de empleados";
+            // 
+            // dataGridViewEmployees
+            // 
+            this.dataGridViewEmployees.AllowUserToAddRows = false;
+            this.dataGridViewEmployees.AllowUserToDeleteRows = false;
+            this.dataGridViewEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployees.Location = new System.Drawing.Point(264, 74);
+            this.dataGridViewEmployees.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewEmployees.Name = "dataGridViewEmployees";
+            this.dataGridViewEmployees.ReadOnly = true;
+            this.dataGridViewEmployees.RowHeadersWidth = 51;
+            this.dataGridViewEmployees.RowTemplate.Height = 24;
+            this.dataGridViewEmployees.Size = new System.Drawing.Size(555, 303);
+            this.dataGridViewEmployees.TabIndex = 14;
+            // 
+            // dtLocations
+            // 
+            this.dtLocations.DataSetName = "dtLocations";
+            this.dtLocations.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // locationsBindingSource
+            // 
+            this.locationsBindingSource.DataMember = "locations";
+            this.locationsBindingSource.DataSource = this.dtLocations;
+            // 
+            // locationsTableAdapter
+            // 
+            this.locationsTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnRestablecer
+            // 
+            this.btnRestablecer.Location = new System.Drawing.Point(61, 238);
+            this.btnRestablecer.Name = "btnRestablecer";
+            this.btnRestablecer.Size = new System.Drawing.Size(75, 40);
+            this.btnRestablecer.TabIndex = 15;
+            this.btnRestablecer.Text = "Restablecer";
+            this.btnRestablecer.UseVisualStyleBackColor = true;
+            this.btnRestablecer.Click += new System.EventHandler(this.btnRestablecer_Click);
+            // 
+            // EmployeeWFLinq
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 411);
+            this.Controls.Add(this.btnRestablecer);
+            this.Controls.Add(this.dataGridViewEmployees);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxCiudad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "EmployeeWFLinq";
+            this.Text = "Employees con LINQ";
+            this.Load += new System.EventHandler(this.EmployeeWF_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtLocations)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locationsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxCiudad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridViewEmployees;
+        private dtLocations dtLocations;
+        private System.Windows.Forms.BindingSource locationsBindingSource;
+        private dtLocationsTableAdapters.locationsTableAdapter locationsTableAdapter;
+        private System.Windows.Forms.Button btnRestablecer;
+    }
+}
---
+++ AppEmpleados/EmployeeWFLinq.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AppEmpleados
+{
+    public partial class EmployeeWFLinq : Form
+    {
+        public EmployeeWFLinq()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeWF_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dtLocations.locations' Puede moverla o quitarla según sea necesario.
+            this.locationsTableAdapter.Fill(this.dtLocations.locations);
+            MejorasInterfaz();
+            CargarDataGrid();
+        }
+        private void CargarDataGrid()
+        {
+            ClaseEmployeesDataContext dc = new ClaseEmployeesDataContext();
+
+            var data = from employees in dc.employees
+                       join d in dc.departments
+                       on employees.department_id equals d.department_id
+                       join l in dc.locations
+                       on d.location_id equals l.location_id
+                       select new { employees.employee_id, employees.first_name, employees.last_name, l.city };
+
+            dataGridViewEmployees.DataSource = data.ToList();
+
+            //var dataBis = from employees in dc.employees
+            //              where employees.employee_id == 103
+            //            select employees;
+            //employees empleado = dataBis.FirstOrDefault();
+            //empleado.salary = 100;
+            //empleado.first_name = "Denis";
+            //dc.SubmitChanges();
+        }
+
+        private void MejorasInterfaz()
+        {
+            // Establece la paleta de colores
+            this.BackColor = Color.LightGray; // Cambia el color de fondo a gris claro
+
+            DataGridViewRow row = this.dataGridViewEmployees.RowTemplate;
+            row.DefaultCellStyle.BackColor = Color.Bisque;
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            FiltrarPorNombre(txtNombre.Text);
+        }
+
+        private void txtApellidos_TextChanged(object sender, EventArgs e)
+        {
+            FiltrarPorApellido(txtApellidos.Text);            
+        }
+
+        private void comboBoxCiudad_SelectedValueChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ComboBox comboBox = (System.Windows.Forms.ComboBox)sender;
+
+            if (comboBox != null)
+                if (comboBox.Items.Count > 0)
+                    if (comboBox.SelectedValue != null)
+                    {
+                        int idLocationSelected = Convert.ToInt32(comboBox.SelectedValue.ToString());
+                        RowFilterInt(idLocationSelected);
+                    }
+
+            MejorasInterfaz();
+        }
+
+        private void FiltrarPorNombre(string nombre)
+        {
+            ClaseEmployeesDataContext dc = new ClaseEmployeesDataContext();
+            var data = from employees in dc.employees
+                       join d in dc.departments
+                       on employees.department_id equals d.department_id
+                       join l in dc.locations
+                       on d.location_id equals l.location_id
+                       where employees.first_name.ToLower().Contains(nombre)
+                       select new { employees.employee_id, employees.first_name, employees.last_name, l.city };
+
+            dataGridViewEmployees.DataSource = data.ToList();
+        }
+
+        private void FiltrarPorApellido(string apellido)
+        {
+            ClaseEmployeesDataContext dc = new ClaseEmployeesDataContext();
+            var data = from employees in dc.employees
+                       join d in dc.departments
+                       on employees.department_id equals d.department_id
+                       join l in dc.locations
+                       on d.location_id equals l.location_id
+                       where employees.last_name.ToLower().Contains(apellido)
+                       select new { employees.employee_id, employees.first_name, employees.last_name, l.city };
+
+            dataGridViewEmployees.DataSource = data.ToList();
+        }
+
+        private void RowFilterInt(int idFiltro)
+        {
+            ClaseEmployeesDataContext dc = new ClaseEmployeesDataContext();
+            var data = from employees in dc.employees
+                       join d in dc.departments
+                       on employees.department_id equals d.department_id
+                       join l in dc.locations
+                       on d.location_id equals l.location_id
+                       where l.location_id == idFiltro
+                       select new { employees.employee_id, employees.first_name, employees.last_name, l.city };
+
+            dataGridViewEmployees.DataSource = data.ToList();
+        }
+
+        private void btnRestablecer_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = String.Empty;
+            txtApellidos.Text = String.Empty;
+            comboBoxCiudad.SelectedIndex = -1;
+            CargarDataGrid();
+        }
+    }
+}
